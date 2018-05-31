11161e49567cec9de4f01ba25f1b96c9bd7aa1f0
Scott McCormack <scott.mccormack@outlook.com>
2018-05-31 09:17:52 +0800

    Test that error messages are cleared on input

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -15,6 +15,9 @@ class ItemValidationTest(FunctionalTest):
                     raise e
                 time.sleep(0.5)
 
+    def get_error_element(self):
+        return self.browser.find_element_by_css_selector('.has-error')
+
     def test_cannot_add_empty_list_items(self):
         # Scott goes to the home page and accidentally tries to submit an empty list item.
         # He hits Enter on the empty input box
@@ -67,6 +70,28 @@ class ItemValidationTest(FunctionalTest):
 
         # He sees a helpful error message
         self.wait_for(lambda: self.assertEqual(
-            self.browser.find_element_by_css_selector('.has-error').text,
+            self.get_error_element().text,
             "You've already got this in your list"
+        ))
+
+    def test_error_messages_are_cleared_on_input(self):
+        # Scott starts a list and causes a validation error
+        self.browser.get(self.live_server_url)
+        self.get_item_input_box().send_keys('Banter too thick')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Banter too thick')
+        self.get_item_input_box().send_keys('Banter too thick')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+
+        # He sees a helpful error message
+        self.wait_for(lambda: self.assertTrue(
+            self.get_error_element().is_displayed()
+        ))
+
+        # He starts typing in the input box to clear the error
+        self.get_item_input_box().send_keys('a')
+
+        # He is pleased to see that the error message disappears
+        self.wait_for(lambda: self.assertFalse(
+            self.get_error_element().is_displayed()
         ))
